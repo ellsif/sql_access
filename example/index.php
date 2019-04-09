cb0adf635bbf23a5479d949a556e60535195736a
HATASU SAKAI <hatasu@ellsif.com>
2019-04-09 22:29:38 +0900

    add example

---
+++ example/index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * SqlAccessor利用サンプル
+ */
+require_once dirname(dirname(__FILE__)) . '/vendor/autoload.php';
+use ellsif\sql_access\SqlAccessor;
+use ellsif\sql_access\SqlLogger;
+use ellsif\sql_access\SqlManage;
+
+$sqlAccessor = new SqlAccessor(
+    'mysql:host=127.0.0.1;port=13316;dbname=SqlLog;charset=utf8',
+    'root',
+    'root'
+);
+$sqlLogger = new SqlLogger($sqlAccessor);
+
+// ExampleCustomerテーブル作成
+$sqlAccessor->createTable(SqlManage::getSql(SqlManage::CREATER_EXAMPLE_CUSTOMER), []);
+
+// カスタマー保存
+$sqlAccessor->insert(
+    SqlManage::getSql(SqlManage::INSERT_EXAMPLE_CUSTOMER),
+    [
+        'name' => 'Test Taro',
+        'address' => '000-0000 Tokyo',
+        'email' => 'test@example.com',
+    ],
+    SqlManage::getSetting(SqlManage::INSERT_EXAMPLE_CUSTOMER)
+);
